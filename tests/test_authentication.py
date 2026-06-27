1aa267f4fbe57a2224e4ff1c7c56541b820b99b6
gituser357 <sergey.dv520@gmail.com>
2026-06-27 22:58:22 +0300

    Маркировки pytest

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,8 +1,7 @@
 from http import HTTPStatus
 
 from clients.authentication.authentication_client import get_authentication_client
-from clients.authentication.authentication_schema import LoginResponseSchema
-from clients.private_http_builder import AuthenticationUserSchema
+from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema
 from tools.assertions.authentication import assert_login_response
@@ -12,17 +11,16 @@ from tools.assertions.schema import validate_json_schema
 
 def test_login():
     public_users_client = get_public_users_client()
-    auth_client = get_authentication_client()
+    authentication_client = get_authentication_client()
 
     create_user_request = CreateUserRequestSchema()
-    create_user_response = public_users_client.create_user(create_user_request)
+    public_users_client.create_user(create_user_request)
 
-    authentication_user = AuthenticationUserSchema(
+    login_request = LoginRequestSchema(
         email=create_user_request.email,
         password=create_user_request.password
     )
-
-    login_response = auth_client.login_api(authentication_user) # здесь выполнили аутентификацию
+    login_response = authentication_client.login_api(login_request)
     login_response_data = LoginResponseSchema.model_validate_json(login_response.text)
 
     assert_status_code(login_response.status_code, HTTPStatus.OK)
