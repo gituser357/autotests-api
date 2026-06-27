755d1d0f797257c2011398cc972f3ebc686070e7
gituser357 <sergey.dv520@gmail.com>
2026-06-28 00:19:07 +0300

    pytest skip, skipif, xfail

--- probus.py
+++ probus.py
@@ -1,2 +1,14 @@
-import pydantic
-print(pydantic.__version__)
+import pytest
+
+@pytest.mark.registration
+def test_user_registration():
+    pass
+
+@pytest.mark.smoke
+def test_user_login():
+    pass
+
+@pytest.mark.registration
+@pytest.mark.regression
+def test_password_reset():
+    pass
